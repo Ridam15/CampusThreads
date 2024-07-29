5651f1efdc6d4b015e5e393565424bd487ea09f4
Ridam Chhapiya <ridamchhapiya12@gmail.com>
2024-07-29 15:55:49 +0530

    adding Client/frontend

---
+++ Client/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0, transform: 'scale(0.9)' },
+          '100%': { opacity: 1, transform: 'scale(1)' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.3s ease-in-out',
+      },
+    },
+  },
+  plugins: [],
+}
